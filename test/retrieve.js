86ecc496069190940928c686855556eaee16c939
Neamar <neamar@neamar.fr>
2013-10-31 16:26:49 +0100

    Linter

--- test/retrieve.js
+++ test/retrieve.js
@@ -6,7 +6,6 @@ var retrieve = require('../lib/provider-gmail/helpers/retrieve.js');
 
 describe("Retrieve code", function () {
   it("should get all mails", function (done) {
-    this.timeout(5000);
     var mails = [];
     retrieve(config.test_refresh_token, config.test_account,new Date(1970), function(mail) {
       mails.push(mail);
@@ -20,34 +19,30 @@ describe("Retrieve code", function () {
     });
   });
 
-
   it("should list mail modified in future date", function (done) {
-    this.timeout(5000);
     var mails = [];
     retrieve(config.test_refresh_token, config.test_account, new Date(2020, 9, 22), function(mail) {
       mails.push(mail);
-      },
-      function (err) {
-        if(err) {
-          throw err;
-        }
-        mails.should.have.lengthOf(0);
-        done();
+    },
+    function (err) {
+      if(err) {
+        throw err;
+      }
+      mails.should.have.lengthOf(0);
+      done();
     });
   });
 
   it("should list mail modified after specified date", function (done) {
-    this.timeout(5000);
     var mails = [];
     retrieve(config.test_refresh_token, config.test_account, new Date(2013, 9, 22), function(mail) {
       mails.push(mail);
-      },
-      function (err) {
-        if(err) {
-          throw err;
-        }
-        mails.should.have.lengthOf(6);
-        done();
+    }, function (err) {
+      if(err) {
+        throw err;
+      }
+      mails.should.have.lengthOf(6);
+      done();
     });
   });
 });
