0a142ef5c632f82077a1896580b767b767899f08
Neamar <neamar@neamar.fr>
2013-11-08 10:28:39 +0100

    Updated test-imap.js

--- test-imap.js
+++ test-imap.js
@@ -3,8 +3,10 @@
 var retrieve = require('./lib/provider-gmail/helpers/retrieve.js');
 var config = require('./config/configuration.js');
 
+var mails = 1;
 var cb = function(datas) {
-  console.log('----------');
+  mails += 1;
+  console.log('----------' + mails);
   console.log("URL:", datas.actions.show);
   console.log("Id:", datas.metadatas.id);
   console.log("tId:", datas.metadatas.threadid);
@@ -14,4 +16,4 @@ var cb = function(datas) {
   console.log("Labels:", datas.metadatas.labels);
 };
 
-retrieve(config.test_refresh_token, config.test_account, 1, cb, process.exit);
+retrieve(config.test_refresh_token, config.test_account, new Date(1970), cb, process.exit);
