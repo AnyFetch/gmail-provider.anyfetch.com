73285f032b23ae1ec790a4abb1d5a41f3a81324e
Neamar <neamar@neamar.fr>
2013-11-12 17:53:12 +0100

    Logging for ur life

--- test-imap.js
+++ test-imap.js
@@ -8,18 +8,20 @@ var cb = function(datas) {
   mails += 1;
   console.log('----------' + mails);
   console.log("URL:", datas.actions.show);
-  console.log("Id:", datas.metadatas.id);
-  console.log("tId:", datas.metadatas.threadid);
-  console.log("From:", datas.metadatas.from);
-  console.log("To:", datas.metadatas.to);
-  console.log("Subject:", datas.metadatas.subject);
-  console.log("Text:", datas.metadatas.content);
-  console.log("Labels:", datas.metadatas.labels);
+  // console.log("Id:", datas.metadatas.id);
+  // console.log("tId:", datas.metadatas.threadid);
+  // console.log("From:", datas.metadatas.from);
+  // console.log("To:", datas.metadatas.to);
+  // console.log("Subject:", datas.metadatas.subject);
+  // console.log("Text:", datas.metadatas.content);
+  // console.log("Labels:", datas.metadatas.labels);
 };
 
 retrieve(config.test_refresh_token, config.test_account, new Date(1970), cb, function(err) {
   if(err) {
     throw err;
   }
+
+  console.log("Bye.");
   process.exit();
 });
