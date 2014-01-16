b0267ae9c8923b2e6971d44a474d43b1b2e68667
Florian Rossiaud <florian.rossiaud@gmail.com>
2014-01-16 13:34:16 +0100

    Fixing gmail retrieve function : some test have to be done

--- test-imap.js
+++ test-imap.js
@@ -2,11 +2,13 @@
 
 var retrieve = require('./lib/provider-gmail/helpers/retrieve.js');
 var config = require('./config/configuration.js');
+var fs = require('fs');
 
 var mails = 0;
 var cb = function(datas) {
   mails += 1;
   console.log('----------' + mails);
+  //fs.appendFile("log-imap-debug.txt", '----------' + mails + "\n");
   //console.log("URL:", datas.actions.show);
   //console.log("uId:", datas.datas.uid);
   //console.log("Id:", datas.datas.id);
@@ -18,7 +20,7 @@ var cb = function(datas) {
   // console.log("Labels:", datas.metadatas.labels);
 };
 
-retrieve(config.test_refresh_token, config.test_account, 5, cb, function(err) {
+retrieve(config.test_refresh_token, config.test_account, 1, cb, function(err) {
   if(err) {
     throw err;
   }
